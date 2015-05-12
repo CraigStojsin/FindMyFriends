a6102736267d2a44b1cd38e9ef0315ee32cf6f1f
CraigStojsin <stojsin@outlook.com>
2015-05-12 11:39:05 -0400

    Initial Commit

---
+++ FindMyFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindMyFriends
+//
+//  Created by Craig Stojsin on 2015-05-12.
+//  Copyright (c) 2015 CraigCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindMyFriends/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindMyFriends
+//
+//  Created by Craig Stojsin on 2015-05-12.
+//  Copyright (c) 2015 CraigCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
